d7e7b2e2a9015bfe0c541a1a4990d68afcaca2ca
Ihor Rohovets <freego555@gmail.com>
2024-09-30 21:06:44 +0300

    Server. Chapter 7. Relations Between Models. One2many. Add new field offer_ids to estate.property model

--- addons/estate/models/estate_property.py
+++ addons/estate/models/estate_property.py
@@ -32,3 +32,4 @@ class Property(models.Model):
   user_id = fields.Many2one('res.users', string='Salesperson', default=lambda self: self.env.uid)
   partner_id = fields.Many2one('res.partner', string='Buyer', copy=False)
   tag_ids = fields.Many2many('estate.property.tag', string='Tags')
+  offer_ids = fields.One2many('estate.property.offer', 'property_id', string='Offers')
