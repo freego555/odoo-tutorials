0352bbedeb0115eedcc21d289925433d675902dc
Ihor Rohovets <freego555@gmail.com>
2024-09-30 16:23:07 +0300

    Server. Chapter 7. Relations Between Models. Many2many. Add new field tag_ids to estate.property model

--- addons/estate/models/estate_property.py
+++ addons/estate/models/estate_property.py
@@ -31,3 +31,4 @@ class Property(models.Model):
   property_type_id = fields.Many2one("estate.property.type", string="Property Type")
   user_id = fields.Many2one("res.users", string="Salesperson", default=lambda self: self.env.uid)
   partner_id = fields.Many2one("res.partner", string="Buyer", copy=False)
+  tag_ids = fields.Many2many("estate.property.tag", string="Tags")
